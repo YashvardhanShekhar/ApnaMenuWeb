d753a40237cf24f0428a141c3898c4ccefceb51a
Yashvardhan Shekhar <yashvardhanshekhar44@gmail.com>
2025-10-11 16:13:28 +0530

    fixed env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,17 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, doc, getDoc } from "firebase/firestore";
 
-const firebaseConfig = {
-	apiKey: "AIzaSyCd8VzrChLGC-uPIFYMI3Ow3ALM5PbwF0w",
-	authDomain: "apnamenu-snap.firebaseapp.com",
-	databaseURL:
-		"https://apnamenu-snap-default-rtdb.asia-southeast1.firebasedatabase.app",
-	projectId: "apnamenu-snap",
-	storageBucket: "apnamenu-snap.firebasestorage.app",
-	messagingSenderId: "1054196927034",
-	appId: "1:1054196927034:web:9728feb0fc9b817d8dc303",
-	measurementId: "G-4RSPQK47KD",
+export const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
